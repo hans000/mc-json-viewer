ab87beff9489222315e3ab5e4abc2bb5343dc5d6
hans <haima_kc@163.com>
2020-08-23 13:53:02 +0800

    refactor: 调整资源文件

--- src/extension.tsx
+++ src/extension.tsx
@@ -8,19 +8,19 @@ import TreeProvider from './logic/TreeProvider';
 import React from 'react';
 
 export function activate(context: vscode.ExtensionContext) {
-	const openviewer = vscode.commands.registerCommand('mc-json-viewer.openviewer', () => {
-			const lang = fs.readFileSync(path.join(context.extensionPath, './src/assets/1.16.zh_cn.json')).toString();
-			const tpl = getWebViewContent(context, './src/assets/template.html');
+	const openviewer = vscode.commands.registerCommand('mc-json-viewer.view', () => {
+			const lang = fs.readFileSync(path.join(context.extensionPath, './assets/1.16.zh_cn.json')).toString();
+			const tpl = getWebViewContent(context, './assets/template.html');
 			const editor = vscode.window.activeTextEditor;
 			if (editor) {
 				const paths = getActivePaths(editor, 'advancements');
 				const treeProvider = new TreeProvider(editor, paths, 'advancements');
 				const data = treeProvider.getData();
+				const panel = vscode.window.createWebviewPanel('mc-json-viewer', '预览', vscode.ViewColumn.Beside, {});
 				if (!data[0]) {
 					vscode.window.showWarningMessage('当前目录无法生成进度');
 					return;
 				}
-				const panel = vscode.window.createWebviewPanel('mc-json-viewer', '预览', vscode.ViewColumn.Beside, { enableScripts: true });
 				const newHtml = tpl.replace('{{}}', renderToString(<McTree data={data} lang={JSON.parse(lang)} />));
 				panel.webview.html = newHtml;
 			}
